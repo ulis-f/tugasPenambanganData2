26714da6ca08cd62704545ebed5c71c5b3f34f2d
stephenhadi <stephenhadi123@gmail.com>
2021-06-08 15:24:52 +0700

    update

--- main.py
+++ main.py
@@ -6,4 +6,22 @@ Created on Mon Jun  7 12:31:34 2021
 
 """
 
+import cv2
+import os
+import matplotlib.pyplot as plt
 
+def main():
+    directory = "images"
+    img1 = cv2.imread(os.path.join(directory,'img1.jpg'))
+    img1 = cv2.cvtColor(img1, cv2.COLOR_BGR2RGB)
+    
+    sift = cv2.SIFT_create()
+    keypoint, description = sift.detectAndCompute(img1,None)
+    img_keypoints=cv2.drawKeypoints(img1,keypoint,img1)
+    
+    plt.figure(figsize=(64 , 64))    
+    plt.imshow(img_keypoints); plt.show()
+    cv2.imwrite(os.path.join(directory,'img1_keypoints.jpg'),img_keypoints)
+    
+if __name__ == '__main__':
+    main()
